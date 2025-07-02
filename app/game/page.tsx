b4e85007d5424448d732bb457c9ae9fb7ea4ef08
HamzaAhmad6292 <hamzaahmad6292@gmail.com>
2025-07-03 02:20:15 +0500

    added

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,15 +1,11 @@
-// app/game/page.tsx or pages/game.tsx
 export default function GamePage() {
   return (
-    <div className="w-screen h-screen overflow-hidden bg-black">
+    <div className="w-screen h-screen bg-black">
       <iframe
-        frameBorder="0"
-        src="https://itch.io/embed/3691071"
-        width="100%"
-        height="100%"
-      >
-        <a href="https://hamza010101.itch.io/monkeyonsol">Monkeyonsol by Hamza010101</a>
-      </iframe>
+        src="https://hamza010101.itch.io/monkeyonsol?secret=5BJcYSDF2aOVO42YEbIHoSaD8"
+        allow="fullscreen"
+        className="w-full h-full border-none"
+      />
     </div>
-  )
+  );
 }
