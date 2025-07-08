1da337273eb3c0eaadcd0a32f0c5ad3e1fc8b07b
HamzaAhmad6292 <hamzaahmad6292@gmail.com>
2025-07-08 13:09:27 +0500

    final game added

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 export default function GamePage() {
   const [isMobile, setIsMobile] = useState(false);
   const [loaded, setLoaded] = useState(false);
-  const gameURL = 'https://peppy-fairy-cb97c0.netlify.appsgit ';
+  const gameURL = 'https://peppy-fairy-cb97c0.netlify.apps';
 
   useEffect(() => {
     const mobileRegex = /iPhone|iPad|iPod|Android/i;
