bee5a53023f8831565dfc1dd9127db2f3efd9a5c
HamzaAhmad6292 <hamzaahmad6292@gmail.com>
2025-07-17 00:58:15 +0500

    first commit

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 export default function GamePage() {
   const [isMobile, setIsMobile] = useState(false);
   const [loaded, setLoaded] = useState(false);
-  const gameURL = 'https://monumental-fairy-c081a6.netlify.app';
+  const gameURL = 'https://gregarious-clafoutis-9ebc5c.netlify.app';
 
   // useEffect(() => {
   //   const mobileRegex = /iPhone|iPad|iPod|Android/i;
